48c90587ff8d4d58ec2420dfbb537006d9cd5c29
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-03-24 20:56:16 -0400

    Add QueryPartsTests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,37 @@
+from django.db import connection
 from django.test import TestCase
 
-from django_mongodb_extensions.debug_toolbar.panels.mql.utils import parse_query_args
+from django_mongodb_extensions.debug_toolbar.panels.mql.utils import (
+    QueryParts,
+    parse_query_args,
+)
+
+
+class QueryPartsTests(TestCase):
+    def test_initialization(self):
+        query_dict = {"mql": "db.auth_user.aggregate([])", "alias": "default"}
+        db = connection.database
+        collection = db["auth_user"]
+        query_parts = QueryParts(
+            query_dict=query_dict,
+            alias="default",
+            mql_string="db.auth_user.aggregate([])",
+            connection=connection,
+            db=db,
+            collection=collection,
+            collection_name="auth_user",
+            operation="aggregate",
+            args_list=[[]],
+        )
+        self.assertEqual(query_parts.query_dict, query_dict)
+        self.assertEqual(query_parts.alias, "default")
+        self.assertEqual(query_parts.mql_string, "db.auth_user.aggregate([])")
+        self.assertEqual(query_parts.collection_name, "auth_user")
+        self.assertEqual(query_parts.operation, "aggregate")
+        self.assertEqual(query_parts.args_list, [[]])
+        self.assertIs(query_parts.connection, connection)
+        self.assertIs(query_parts.db, db)
+        self.assertIs(query_parts.collection, collection)
 
 
 class ParseQueryArgsTests(TestCase):
